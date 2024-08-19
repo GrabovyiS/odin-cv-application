7f311be8b681aed3cfdca306dcb40e881bcfed19
GrabovyiS <graboviys@yandex.ru>
2024-08-19 12:41:35 +0300

    feat: general info form component

--- src/components/forms/GeneralInfoForm/GeneralInfoForm.jsx
+++ src/components/forms/GeneralInfoForm/GeneralInfoForm.jsx
@@ -1,17 +1,48 @@
 import Input from "../../formControls/Input/Input";
 import Button from "../../formControls/Button/Button";
+import { useState } from "react";
 
 function GeneralInfoForm() {
-  const [formData, setFormData] = "";
+  const [formData, setFormData] = useState({ name: "", email: "", phone: "" });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <form action="">
-      <Input type="name" name={"name"} label={"Name:"} />
-      <Input type="email" name={"email"} label={"Email:"} />
-      <Input type="tel" name={"phone"} label={"Phone:"} />
-      <Button>Text</Button>
-      <Button option="danger">Text</Button>
-      <Button option="success">Text</Button>
+    <form className="form-container" onSubmit={handleSubmit} action="">
+      <Input
+        onChange={handleChange}
+        value={formData.name}
+        type="name"
+        name="name"
+        label="Name:"
+      />
+      <Input
+        onChange={handleChange}
+        value={formData.email}
+        type="email"
+        name="email"
+        label="Email:"
+      />
+      <Input
+        onChange={handleChange}
+        value={formData.phone}
+        type="tel"
+        name="phone"
+        label="Phone:"
+      />
+      <div className="form-buttons">
+        <Button type="submit" option="success">
+          Submit
+        </Button>
+        <Button>Edit</Button>
+      </div>
     </form>
   );
 }
