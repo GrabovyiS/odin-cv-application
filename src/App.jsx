be1d06e4f324734eda3138b8ff44794c1e93c6c7
GrabovyiS <graboviys@yandex.ru>
2024-08-19 15:57:38 +0300

    feat: education form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import GeneralInfoForm from "./components/forms/GeneralInfoForm/GeneralInfoForm";
+import EducationForm from "./components/forms/EducationForm/EducationForm";
 import "./App.css";
 
 function App() {
-  return <GeneralInfoForm />;
+  return (
+    <div>
+      <GeneralInfoForm />
+      <EducationForm />
+    </div>
+  );
 }
 
 export default App;
