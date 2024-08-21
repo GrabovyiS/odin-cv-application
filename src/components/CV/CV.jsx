a63ea0cbaf6b438442e382bd4a20d73e6a4967ef
GrabovyiS <graboviys@yandex.ru>
2024-08-21 11:59:43 +0300

    feat: calculate total work experience time

--- src/components/CV/CV.jsx
+++ src/components/CV/CV.jsx
@@ -51,25 +51,91 @@ function CV({ general, educations, workExperiences }) {
   if (!workExperiences) {
     workExperienceContent = <></>;
   } else {
+    workExperiences.sort((workExperience1, workExperience2) => {
+      const startDate1 = new Date(workExperience1["start-date"]);
+      const startDate2 = new Date(workExperience2["start-date"]);
+
+      return startDate1 - startDate2;
+    });
+
+    const workRanges = [
+      {
+        "start-date": new Date(workExperiences[0]["start-date"]),
+        "end-date": new Date(workExperiences[0]["end-date"]),
+      },
+    ];
+    let currentWorkRangeIndex = 0;
+
+    for (let i = 1; i < workExperiences.length; i++) {
+      let currentWorkRange = workRanges[currentWorkRangeIndex];
+
+      let experienceStartDate = new Date(workExperiences[i]["start-date"]);
+
+      let experienceEndDate =
+        workExperiences[i]["end-date"] === ""
+          ? new Date()
+          : new Date(workExperiences[i]["end-date"]);
+
+      let workRangeEndDate =
+        currentWorkRange["end-date"] === ""
+          ? new Date()
+          : new Date(currentWorkRange["end-date"]);
+
+      if (
+        experienceStartDate <= workRangeEndDate &&
+        experienceEndDate > workRangeEndDate
+      ) {
+        workRanges[currentWorkRangeIndex]["end-date"] = experienceEndDate;
+      } else if (experienceStartDate > workRangeEndDate) {
+        workRanges.push({
+          "start-date": experienceStartDate,
+          "end-date": experienceEndDate,
+        });
+        currentWorkRangeIndex++;
+      }
+    }
+
+    console.log(workRanges);
+
+    let totalMonths = 0;
+    let totalYears = 0;
+
+    for (let i = 0; i < workRanges.length; i++) {
+      const startDate = workRanges[i]["start-date"];
+      const endDate = workRanges[i]["end-date"];
+
+      let currentRangeMonths = endDate.getMonth() - startDate.getMonth();
+      if (currentRangeMonths < 12) {
+        // Round it up, so that 1 week of work still counts as a month
+        // and so that jan2020-jan2020 counts as a month
+        currentRangeMonths++;
+      }
+      currentRangeMonths +=
+        (endDate.getFullYear() - startDate.getFullYear()) * 12;
+
+      totalMonths += currentRangeMonths;
+    }
+
+    totalYears = Math.floor(totalMonths / 12);
+    totalMonths = totalMonths - totalYears * 12;
+
     workExperienceContent = (
       <>
         <h2>Work Experience</h2>
+        <p>
+          {totalYears} years {totalMonths} months
+        </p>
         {workExperiences.map((workExperience) => {
           return (
             <div key={workExperience.id} className="cv-info">
-              <h3>
-                {workExperience.title} in {workExperience.company}
-              </h3>
+              <h3>{workExperience.company}</h3>
+              <h4>{workExperience.title}</h4>
               {workExperience.responsibilities && (
                 <>
-                  <h4>Responsibilities:</h4>
                   <p>{workExperience.responsibilities}</p>
                 </>
               )}
-              <p className="cv-dates">
-                From {workExperience["start-date"]} to
-                {workExperience["end-date"]}
-              </p>
+              <p className="cv-dates">{}</p>
             </div>
           );
         })}
